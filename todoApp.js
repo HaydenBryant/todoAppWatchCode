130cde139ecc6e9a6a25ea13ce498115f1e369f3
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-03-30 16:40:27 +0000

    version 7

--- todoApp.js
+++ todoApp.js
@@ -56,5 +56,19 @@ var todoList = {
                 this.todos[i].completed = true;
             }
         }
+        
+        this.displayTodos();
     }
 };
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+
+displayTodosButton.addEventListener('click', function(){
+    todoList.displayTodos();
+})
+
+toggleAllButton.addEventListener('click', function() {
+    todoList.toggleAll();
+})
