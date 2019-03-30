6c9ea8d00b348ef0a8650b0f8bbfd1136377157b
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-03-30 23:35:05 +0000

    version 10

--- todoApp.js
+++ todoApp.js
@@ -58,10 +58,8 @@ var handlers = {
         changeTodoTextInput.value = '';
         view.displayTodos();
     },
-    deleteTodo: function() {
-        var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-        deleteTodoPositionInput.value = '';
+    deleteTodo: function(position) {
+        todoList.deleteTodo(position);
         view.displayTodos();
     },
     toggleCompleted: function() {
@@ -78,10 +76,8 @@ var handlers = {
 
 var view = {
     displayTodos: function() {
-
         var todosUl = document.querySelector("ul");
         todosUl.innerHTML = "";
-
         for (var i = 0; i < todoList.todos.length; i++) {
             var todoLi = document.createElement("li");
             var todo = todoList.todos[i];
@@ -92,11 +88,31 @@ var view = {
             }
             else {
                 todoTextWithCompletion = '( ) ' + todo.todoText;
-
             }
 
+            todoLi.id = i;
             todoLi.textContent = todoTextWithCompletion;
+            todoLi.appendChild(this.createDeleteButton());
             todosUl.appendChild(todoLi);
         }
+    },
+    createDeleteButton: function() {
+        var deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'deleteButton';
+        return deleteButton;
+    },
+    setUpEventListeners: function() {
+        var todosUl = document.querySelector('ul');
+
+        todosUl.addEventListener('click', function(event) {
+            var elementClicked = event.target;
+
+            if (elementClicked.className === 'deleteButton') {
+                handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+            }
+        });
     }
 };
+
+view.setUpEventListeners();
